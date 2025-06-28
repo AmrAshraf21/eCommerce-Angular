5364980467245288f535042e803fb1c951c0a4f4
amr <amrashraf314@gmail.com>
2025-06-28 20:59:56 +0300

    after making  ng build to reflect changes

---
+++ docs/chunk-BTRQUF73.js
@@ -0,0 +1,1 @@
+import{Ga as n,ab as o,bb as a,pb as i}from"./chunk-WXET5CKO.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-not-found"]],decls:2,vars:0,template:function(t,f){t&1&&(o(0,"p"),i(1,"not-found works!"),a())},encapsulation:2})};export{s as NotFound};
--- docs/chunk-PZ3I3KLZ.js
+++
@@ -1,1 +0,0 @@
-import{$a as o,Fa as n,ab as a,mb as i}from"./chunk-FMD6QGD7.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-not-found"]],decls:2,vars:0,template:function(t,f){t&1&&(o(0,"p"),i(1,"not-found works!"),a())},encapsulation:2})};export{s as NotFound};
